b9d8dd0168e7bf440501c984f363314d0073fda5
Paddy Byrne <paddyb81@gmail.com>
2014-10-25 13:38:11 +0100

    Gigs data model created

--- GiggidyMobileApp/src/ncirl/project/giggidymobileapp/model/GigsModel.java
+++ GiggidyMobileApp/src/ncirl/project/giggidymobileapp/model/GigsModel.java
@@ -1,5 +1,37 @@
 package ncirl.project.giggidymobileapp.model;
 
 public class GigsModel {
+	
+	private String gigName;
+	private String venueName;
+	private String gigDate;
+	private String artistImg;
+	public String getGigName() {
+		return gigName;
+	}
+	public void setGigName(String gigName) {
+		this.gigName = gigName;
+	}
+	public String getVenueName() {
+		return venueName;
+	}
+	public void setVenueName(String venueName) {
+		this.venueName = venueName;
+	}
+	public String getGigDate() {
+		return gigDate;
+	}
+	public void setGigDate(String gigDate) {
+		this.gigDate = gigDate;
+	}
+	public String getArtistImg() {
+		return artistImg;
+	}
+	public void setArtistImg(String artistImg) {
+		this.artistImg = artistImg;
+	}
+	
+	
+	
 
 }
